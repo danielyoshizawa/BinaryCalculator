8a4872ca545233bf01f32d2e6ff612cbbeab217b
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-10-14 16:08:50 -0300

    Initial Structure for BinaryCalculator

---
+++ app/src/main/java/com/danielyoshizawa/binarycalculator/app/BinaryCalculator.java
@@ -0,0 +1,15 @@
+package com.danielyoshizawa.binarycalculator.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BinaryCalculator extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_binary_calculator);
+    }
+}
